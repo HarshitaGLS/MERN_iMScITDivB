cf04d44a28d64bc8d4d2ca90d23b026ddae3dfb5
harshita <harshita.mahehswari@glsuniversity.ac.in>
2025-07-17 14:01:14 +0530

    Jul17

---
+++ Day1_17Jul/variable.js
@@ -0,0 +1,12 @@
+// var num1 // JS does this internally (HOISTING)
+console.log(num1)  //undefined
+var num1 = 10 
+let num2 //ES6
+const num3 = 10
+
+console.log(num1) //10
+num1 = "hello"
+
+//number ,string,boolean ,  undefined and null 
+
+//dynmaically typed
